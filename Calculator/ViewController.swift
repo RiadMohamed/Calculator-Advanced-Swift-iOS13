1c9674cf6144bd9b59d44a9611c337ba05540e7b
Riad Mohamed <riaduk1@gmail.com>
2020-02-15 19:51:20 +0200

    L.325
    Add a function to append the number on screen when tapping number buttons.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,12 +11,18 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
+    var currentNumber: String = ""
+    var isFinishedTyping: Bool = true
     
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
+        isFinishedTyping = true
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Could not convert text on screen to number")
+        }
     
     }
 
@@ -24,6 +30,16 @@ class ViewController: UIViewController {
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            if isFinishedTyping {
+                displayLabel.text = numValue
+                isFinishedTyping = false
+            } else {
+                displayLabel.text! += numValue
+            }
+//            currentNumber += numValue
+            
+        }
     
     }
 
